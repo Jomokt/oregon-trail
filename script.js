307dba01175d79b923a334c707375939a36dd3d9
Jomo Thompson <jomokt@gmail.com>
2021-01-15 18:46:19 -0600

    initial coding js

---
+++ script.js
@@ -0,0 +1,39 @@
+class Traveler {
+    constructor (name) {
+        this.name = name
+        this.food = 1
+        this.isHealthy = true
+
+    }  
+  hunt () {
+
+
+  }
+  eat() {
+
+
+  }
+}
+
+class Wagon {
+    constructor (capacity) {
+        this.capacity = capacity
+        this.passengers = []
+        
+      
+    }  
+  getAvailableSeatCount () {
+
+  }
+  join(traveler) {
+
+  }
+  shouldQuarantine () {
+
+  }
+  totalFood () {
+      
+  }
+
+}
+
